6cbc29a1c126b898b197de4686022f34ecfeebef
IhorStoner <60327455+IhorStoner@users.noreply.github.com>
2020-09-19 14:44:35 +0300

    addPropTypes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       <div className='wrapper'>
         {/* <ProductTable></ProductTable> */}
         {/* <Blog></Blog> */}
-        <Slider>
+        <Slider options={{type:'carousel',perView:2,startAt:1,autoplay:1000}}>
           <img src='https://funik.ru/wp-content/uploads/2018/10/0a37dbac85e134cfb3a5-700x394.jpg' height='500' alt='img1'></img>
           <img src='http://www.poleteli.ru/uploads/posts/2015-03/1425394409_wistaria2.jpg' height='500' alt='img2'></img>
           <img src='https://ru.diez.md/wp-content/uploads/2019/08/d79168a24069b3a9722ec91169074f8e-2.jpg' height='500' alt='img3'></img>
--- src/dz2/Slider.js
+++ src/dz2/Slider.js
@@ -3,29 +3,54 @@ import "../../node_modules/@glidejs/glide/dist/css/glide.core.min.css";
 import '../../node_modules/@glidejs/glide/dist/css/glide.theme.min.css';
 import '../../node_modules/@glidejs/glide/dist/glide.min.js';
 import Glide from '@glidejs/glide';
-
+import { Button } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
 
 export default class Slider extends Component {
 
+  state = {
+    config: {
+      type: this.props.options.type ? this.props.options.type : null, // slider or carousel
+      startAt: this.props.options.startAt ? this.props.options.startAt : null, //type number
+      perView: this.props.options.perView ? this.props.options.perView : null, //type number
+      autoplay: this.props.options.autoplay ? this.props.options.autoplay : null, // type number or bool
+    },
+    pause: true,
+  }
+  
   componentDidMount = () => {
-    const config = {
-      type: 'carousel',
-      perView: 2,
-
-    }
-
-    new Glide('.glide',config).mount()
+    this.glide = new Glide('.glide',this.state.config).mount()
   }
 
   componentDidUpdate = () => {
- 
-   
+    this.glide.update(this.state.config);
+  }
+
+  componentWillUnmount = () => {
+    this.glide.destroy();
+  }
+
+  pauseSlider = () => {
+    if(this.state.config.autoplay){
+      this.setState({
+        config: {
+          autoplay: false,
+        }
+      })
+    } else {
+      this.setState({
+        config: {
+          autoplay: true,
+        }
+      })
+    }
   }
 
   render() {
     const images = this.props.children
 
     return (
+      <>
       <div class="glide">
         <div class="glide__track" data-glide-el="track">
           <ul class="glide__slides">
@@ -36,7 +61,6 @@ export default class Slider extends Component {
                 </li>
               ))
             }
-            
           </ul>
         </div>
         <div class="glide__arrows" data-glide-el="controls">
@@ -44,6 +68,15 @@ export default class Slider extends Component {
           <button class="glide__arrow glide__arrow--right" data-glide-dir=">">next</button>
         </div>
       </div>
+      <Button onClick={this.pauseSlider} >{this.state.pause ? 'Pause' : 'Play'}</Button>
+      </>
     )
   }
 }
+
+Slider.propTypes = {
+  type: PropTypes.string,
+  startAt: PropTypes.number,
+  perView: PropTypes.number,
+  autoplay: PropTypes.bool || PropTypes.number,
+};
