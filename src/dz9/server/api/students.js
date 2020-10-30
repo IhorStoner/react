d9b980452d61aff66d269055ef57f42565f30773
IhorStoner <60327455+IhorStoner@users.noreply.github.com>
2020-10-30 16:17:35 +0200

    dz9

--- src/dz9/server/api/students.js
+++ src/dz9/server/api/students.js
@@ -4,12 +4,13 @@ const studentValidator = require('../middlewares/studentValidator')
 const studentsRouter = Router();
 
 //get all students
-studentsRouter
-  .get('/', (req,res) => {
-    const students = JSON.parse(fs.readFileSync(`${__dirname}/students.json`));
-    res.json(students)
-  })
-  .post('/', studentValidator,(req,res) => {
+studentsRouter.get('/', (req,res) => {
+  const students = JSON.parse(fs.readFileSync(`${__dirname}/students.json`));
+  res.json(students)
+})
+
+// add new student
+studentsRouter.post('/', studentValidator,(req,res) => {
     const newStudent = req.body;
     const students = JSON.parse(fs.readFileSync(`${__dirname}/students.json`));
 
@@ -25,7 +26,7 @@ studentsRouter
     res.status(201).send('New student add');
   })
 
-// getById
+// getStudentById
 studentsRouter.get('/:studentId', (req,res) => {
     const studentId = Number(req.params.studentId);
     const students = JSON.parse(fs.readFileSync(`${__dirname}/students.json`));
@@ -37,6 +38,31 @@ studentsRouter.get('/:studentId', (req,res) => {
     } else {
       res.status(200).send(selectedStudent);
     }
-  })
+})
+
+//changeStudentById
+studentsRouter.put('/:studentId',studentValidator, (req,res) => {
+  const newStudentData = req.body;
+  const studentId = Number(req.params.studentId);
+  const students = JSON.parse(fs.readFileSync(`${__dirname}/students.json`));
+  const updatedStudents = students.map(student => student.id === studentId ? { id: studentId, ...newStudentData } : student);
+  fs.writeFileSync(`${__dirname}/students.json`, JSON.stringify(updatedStudents));
+  res.status(200).send('Student updated')
+})
+
+//deleteStudentById
+studentsRouter.delete('/:studentId', (req,res) => {
+  const studentId = Number(req.params.studentId);
+  const students = JSON.parse(fs.readFileSync(`${__dirname}/students.json`));
+  const studentIndex = students.findIndex(student => student.id === studentId);
+
+  if(studentIndex === -1) {
+    return res.status(400).send('Student not found');
+  } else {
+    students.splice(studentIndex,1);
+    fs.writeFileSync(`${__dirname}/students.json`, JSON.stringify(students));
+    return res.status(200).send('Student has been deleted')
+  }
+})
 
 module.exports = studentsRouter;
